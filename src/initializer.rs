0d223e0f88b9979a3e6cd4c0a698862e11158af9
Patrick Song <patricksongzy@gmail.com>
2021-08-29 21:55:26 -0400

    Remove Arc from initializer

--- src/initializer.rs
+++ src/initializer.rs
@@ -4,14 +4,12 @@ use crate::numbers::*;
 
 use rand::Rng;
 
-use std::sync::Arc;
-
 /// A parameter initializer, which intializes parameters based on the input size.
-pub type Initializer = Arc<dyn Fn(Float) -> Float>;
+pub type Initializer = Box<dyn Fn(Float) -> Float>;
 
 /// Creates a He initializer closure.
 pub fn he() -> Initializer {
-    Arc::new(|x| {
+    Box::new(|x| {
         let stddev = (2.0 / x).sqrt();
         rand::thread_rng().gen_range(-stddev..=stddev)
     })
